f34f308f0cc8231d3491534f85a374f2c9109143
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-03-02 11:55:37 +0530

    Create folder strcture

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
+
+// Route imports
+const user = require("./routes/UserRoute");
+
+app.use("/api/v2", user);
+
+// it's for errorHandeling
+app.use(ErrorHandler);
+
+module.exports = app;
